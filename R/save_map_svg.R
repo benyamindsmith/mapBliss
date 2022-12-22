e7e520e781cb5302e6a6ee979a453c54dbd9e1e2
benyamindsmith <benyamindsmith@gmail.com>
2022-12-21 20:35:35 -0500

    save framed map as svg

--- R/save_map_svg.R
+++ R/save_map_svg.R
@@ -5,6 +5,10 @@
 #'@param viz The viz you created with plot_route or plot_flights.
 #'@param svg_name The name of the file you want to call
 #'@param zoom How much you want to zoom in on your map. I find that setting `zoom=3` works best but try it out yourself to see what works.
+#'@param map
+#'@param delay
+#'@param vwidth
+#'@param vheight
 #'@import magick
 #'@import webshot
 #'@import htmlwidgets
@@ -21,7 +25,12 @@
 #'save_map_svg(viz)
 #'## Deleting file for package speifics
 #'file.remove("Rplot.svg")
-
+#'
+#'plot_city_view("Jersalem, IL") |>
+#'  frame_1(title_text="Jerusalem", subtitle_text="City of Gold", subtitle_font="Brush Script MT")|>
+#'  save_map_frame_svg()
+#'## Deleting file for package speifics
+#'file.remove("Rplot.svg")
 
 save_map_svg<-function(viz, svg_name="Rplot.svg", zoom=3){
   # Saving the html generated
@@ -36,6 +45,26 @@ save_map_svg<-function(viz, svg_name="Rplot.svg", zoom=3){
     image_write(svg_name)
 
   file.remove("temp.html")
+  file.remove("Rplot.png")
+  print(paste(svg_name,"written in",getwd()))
+}
+
+save_map_frame_svg<-function(map,
+                             svg_name="Rplot.svg",
+                             delay=10,
+                             vwidth = 992,
+                             vheight = 1000){
+  # Saving the html generated
+
+  map %>%
+  appshot(vwidth = 992,
+          vheight = 1000,
+          delay=delay,
+          file = "Rplot.png") %>%
+    image_read() %>%
+    image_convert(format='svg') %>%
+    image_write(svg_name)
+
   file.remove("Rplot.png")
   print(paste(svg_name,"written in",getwd()))
 }
