87bbfd2c86b8093c35383f7ddc1f10280108f162
benyamindsmith <benjamin@alterclimate.com>
2024-08-24 23:21:22 -0400

    updated documentation

--- R/frames.R
+++ R/frames.R
@@ -77,3 +77,93 @@ frame_1<- function(map,
   }
   shinyApp(ui, server)
 }
+
+
+
+frame_2<- function(map,
+                   title_text="Title",
+                   subtitle_text = "Subtitle",
+                   title_font="Brush Script MT",
+                   subtitle_font = "Trebuchet MS",
+                   frame_width = "100%",
+                   frame_height = 780
+){
+  ui <-
+    fillPage(
+      ## see various ways of including CSS: https://shiny.rstudio.com/articles/css.html
+      tags$head(
+        tags$style(HTML(css_string <- paste0("
+                  #greetings {
+                      top: -8rem;
+                      position: absolute;
+                      z-index: 1000;
+                      background-color: #ffffff;
+                      border-radius: 50%;
+                      width: 300px; /* Adjust size as needed */
+                      height: 300px; /* Adjust size as needed */
+                      display: flex;
+                      justify-content: center;
+                      align-items: center;
+                      overflow: hidden;
+                  }
+
+                  .fancy_border {
+                      border: 25px solid #ffffff; /* Adjust color and width as needed */
+                      border-radius: 50%;
+                      width: 300px; /* Match the width of #greetings */
+                      height: 300px; /* Match the height of #greetings */
+                      display: flex;
+                      justify-content: center;
+                      align-items: center;
+                      overflow: hidden;
+                      position: relative;
+                  }
+
+                  .title_text {
+                      position: absolute;
+                      top: -5rem;
+                      font-family:", title_font, ";
+                      text-align: center;
+                      width: 100%;
+                  }
+
+                  .subtitle_text {
+                      position: absolute;
+                      top: 1rem; /* Adjust position as needed */
+                      font-family: ", subtitle_font, ";
+                      text-align: center;
+                      width: 100%;
+                  }
+
+                  /* Ensure the text fits well within the circular frame */
+                  .title_text, .subtitle_text {
+                      padding: 0 1rem;
+                  }
+      ")
+        )
+        )
+      ),
+      div(class = 'fancy_border', ## set CSS class to style border
+          div(leafletOutput('map',
+                            width= frame_width,
+                            height=frame_height)),
+          div(id = 'greetings',
+              uiOutput('message'))
+
+      ))
+
+
+
+  server <- function(input, output) {
+    output$map <- renderLeaflet({
+      map
+    })
+    ## note that you can also add CSS classes here:
+    output$message <- renderUI(tagList(
+      h1(title_text, class='title_text'),
+      h3(subtitle_text, class='subtitle_text'))
+    )
+
+  }
+  shinyApp(ui, server)
+}
